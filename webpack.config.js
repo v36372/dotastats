4710e138f6816bacd80185353936c27ae51a301f
Tin Nguyen <tin@floatingcube.com>
2017-03-12 14:34:29 +0700

    Init app

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+'use strict';
+var webpack = require('webpack');
+
+const config = {
+  cache: true,
+  entry: './js/main.js',
+  output: {
+      path: __dirname + "/dist",
+      filename: "main.js"
+  },
+  module: {
+    loaders: [
+    { test: /.jsx?$/, loader: 'babel-loader', exclude: /node_modules/, query: { presets: ['es2015', 'stage-0', 'react'], cacheDirectory: true } },
+    { test: /\.css$/, loaders: ['style-loader', 'css-loader'] },
+    { test: /\.scss$/, loaders: ['style-loader', 'css-loader', 'sass-loader'] }
+    ]
+  }
+};
+
+module.exports = config;
